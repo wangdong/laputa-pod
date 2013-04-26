06fa4bc67855bb58ed65d87c9a31170f7529db5f
wangdong <wangdong@kingsoft.com>
2013-04-26 20:13:43 +0800

    cpod入库

---
+++ cpod/cpod.ino
@@ -0,0 +1,141 @@
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+#include <DHT11.h>
+#include <qNode.h>
+
+
+dht11 DHT11;
+RF24 radio(CONF_PIN_CE, CONF_PIN_CSN);
+
+void setup(void)
+{
+	Serial.begin(CONF_BAUD);
+
+	radio.begin();
+	radio.setDataRate(RF24_2MBPS);
+	radio.setPALevel(RF24_PA_MAX);
+	radio.setPayloadSize(CONF_PAYLOAD);
+	radio.setChannel(CONF_CHANNEL);
+	radio.openWritingPipe(CONF_ADDR_CENTER);
+
+	radio.startListening();
+
+	printf_begin();
+	radio.printDetails();
+}
+
+
+
+void loop(void) {
+	radio.powerUp();
+	probeAndSendByDHT11();
+	radio.powerDown();
+	delay(CONF_PROBE_DELAY);
+}
+
+
+bool probeAndSendByDHT11() {
+	if (DHT11.read(CONF_PIN_DHT11) == DHTLIB_OK) {
+		float data[CONF_DATA_COUNT];
+		data[CONF_DATA_TEMP] = (double)DHT11.temperature;
+		data[CONF_DATA_HUM]  = (double)DHT11.humidity;
+		data[CONF_DATA_DEW]  = (double)helper_dewPointFast(DHT11.temperature, DHT11.humidity);
+
+		radio.write((byte*)data, CONF_DATA_SIZE);
+
+		Serial.print("TEMP: ");
+		Serial.println(data[CONF_DATA_TEMP]);
+
+		return true;
+	}
+	return false;
+}
+
+
+//Celsius to Fahrenheit conversion
+double helper_Fahrenheit(double celsius)
+{
+	return 1.8 * celsius + 32;
+}
+
+// fast integer version with rounding
+//int Celcius2Fahrenheit(int celcius)
+//{
+//  return (celsius * 18 + 5)/10 + 32;
+//}
+
+
+//Celsius to Kelvin conversion
+double helper_Kelvin(double celsius)
+{
+	return celsius + 273.15;
+}
+
+// dewPoint function NOAA
+// reference: http://wahiduddin.net/calc/density_algorithms.htm 
+double helper_dewPoint(double celsius, double humidity)
+{
+	// RATIO was originally named A0, possibly confusing in Arduino context
+	double RATIO = 373.15 / (273.15 + celsius);
+	double SUM = -7.90298 * (RATIO - 1);
+	SUM += 5.02808 * log10(RATIO);
+	SUM += -1.3816e-7 * (pow(10, (11.344 * (1 - 1/RATIO ))) - 1) ;
+	SUM += 8.1328e-3 * (pow(10, (-3.49149 * (RATIO - 1))) - 1) ;
+	SUM += log10(1013.246);
+	double VP = pow(10, SUM - 3) * humidity;
+	double T = log(VP/0.61078);   // temp var
+	return (241.88 * T) / (17.558 - T);
+}
+
+// delta max = 0.6544 wrt dewPoint()
+// 5x faster than dewPoint()
+// reference: http://en.wikipedia.org/wiki/Dew_point
+double helper_dewPointFast(double celsius, double humidity)
+{
+	double a = 17.271;
+	double b = 237.7;
+	double temp = (a * celsius) / (b + celsius) + log(humidity/100);
+	double Td = (b * temp) / (a - temp);
+	return Td;
+}
+
+
+/*
+ Copyright (C) 2011 J. Coliz <maniacbug@ymail.com>
+ 
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ version 2 as published by the Free Software Foundation.
+ */
+ 
+/**
+ * @file printf.h
+ *
+ * Setup necessary to direct stdout to the Arduino Serial library, which
+ * enables 'printf'
+ */
+
+#ifndef __PRINTF_H__
+#define __PRINTF_H__
+
+#ifdef ARDUINO
+
+int serial_putc( char c, FILE * ) 
+{
+  Serial.write( c );
+
+  return c;
+} 
+
+void printf_begin(void)
+{
+  fdevopen( &serial_putc, 0 );
+}
+
+#else
+#error This example is only for use on Arduino.
+#endif // ARDUINO
+
+#endif // __PRINTF_H__
+
